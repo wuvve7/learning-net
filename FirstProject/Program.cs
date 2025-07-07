4e55b626bed6ee276d3790f0ae14ea4eedf7ef08
Watin Hamad Lafi Almutairi <Watmutairi@qumc.edu.sa>
2025-07-07 09:39:16 +0300

    Initial commit with .gitignore

---
+++ FirstProject/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Colorfunction("White");
+            Colorfunction("lolo");
+            // This is a simple console application that prints "Hello, World!" to the console.
+            // You can run this application using the .NET CLI or Visual Studio.
+        }
+
+        static void Colorfunction(string life)
+        {
+            var colors = new string[] { "Red", "Green", "Blue", life };
+
+            foreach (var color in colors)
+            {
+                Console.WriteLine(color);
+            }
+        }
+    }
+}
