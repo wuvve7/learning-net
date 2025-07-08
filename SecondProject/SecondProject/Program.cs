d3980dd3adb078105961da215c55480266515673
Watin Hamad Lafi Almutairi <Watmutairi@qumc.edu.sa>
2025-07-08 10:49:47 +0300

    using classes and objects in second project

---
+++ SecondProject/SecondProject/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var myCat = new cat();
+
+        Console.WriteLine("enter your cat name: ");
+        myCat.name = Console.ReadLine();
+        Console.WriteLine("enter your cat age: ");
+        myCat.age = int.Parse(Console.ReadLine());
+        Console.WriteLine("enter your cat color: ");
+        myCat.color = Console.ReadLine();
+        Console.WriteLine("enter your cat gender: ");
+        myCat.gender = Console.ReadLine();
+        
+        myCat.catInfo();
+    }
+    class cat
+    {
+        //fields
+        public string name;
+        public int age;
+        public string color;
+        public string gender;
+
+        //constructor
+        public cat()
+        {
+            name = "Mittens";
+            age = 3;
+            color = "Black";
+            gender = "Male";
+        }
+
+        //functions
+        public void catInfo()
+        {
+            Console.WriteLine("cat name is: {0} , it's age is: {1} , it's color is: {2} , it's gender is: {3}" , name , age, color, gender);
+        }
+
+    }
+}
